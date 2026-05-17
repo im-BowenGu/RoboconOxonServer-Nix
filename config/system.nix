c02ce5d8fd7ae055bc0a688889b8766ef7c61689
im-BowenGu <bowen.gu@abingdon.org.uk>
2026-05-17 20:13:38 +0100

    fix: simplify nginx proxy to fix 400 error

--- config/system.nix
+++ config/system.nix
@@ -100,7 +100,7 @@
   services.nginx = {
     enable = true;
     package = pkgs.nginx;
-    recommendedProxySettings = true;
+    recommendedProxySettings = false;
     virtualHosts."default" = {
       serverName = "_";
       default = true;
@@ -112,9 +112,6 @@
         proxyWebsockets = true;
         extraConfig = ''
           proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
         '';
       };
     };
